2dbcdf276d55fabbeed03d165bfe9cf75fcd533e
Gourav Ranga <gouravranga@arspl.com>
2021-02-10 14:16:50 +0530

    02/10/2021

--- Windows Script/batch.bat
+++ Windows Script/batch.bat
@@ -3,7 +3,7 @@
 :: self execute batch path on boot time : C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 :: comment section
 
- 
+ start chrome https://intra.rathi.com/ http://mantis.rathi.com/mantis/my_view_page.php?refresh=true https://mattermost.rathi.com/java-team/messages/@deepakmangal https://anandrathiitpl.atlassian.net/jira/software/c/projects/OFA/issues/?filter=myopenissues https://gitlab.rathi.com/ofa/releases/sprint/-/tree/OFASprint_43
  start E:\eclipse\eclipse.exe
 
 start C:\starter\TeamViewer.exe
